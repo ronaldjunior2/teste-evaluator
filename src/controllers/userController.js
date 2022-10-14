b714beaeec13d2249f766206bc0b402af095ef3b
ronaldjunior2 <ronaldjunior2@hotmail.com>
2022-10-14 11:32:54 -0300

    feat: first commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,45 +1,45 @@
 import { userHandler } from "../repository/userRepository.js";
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const SECRET = process.env.ACCESS_TOKEN_SECRET;
+const SECRET = process.env.TOKEN_SECRET;
 
-export const signUpHandler = async (req,res) => {
-    const user = res.locals.cleanData;
-    try {
-        const result = await userHandler.userExists(user);
-        
-        if(result === true) return res.sendStatus(409)
-        
-        delete user.confirmPassword;
+export const signUpHandler = async (req, res) => {
+  const user = res.locals.cleanData;
+  try {
+    const result = await userHandler.userExists(user);
 
-        const hashPassword = bcrypt.hashSync(user.password, 10);
+    if (result === true) return res.sendStatus(409);
 
-        const success = await userHandler.signUp(user, hashPassword);
-    
-        if(success) return res.sendStatus(201);
-    } catch (error) {
-        return res.sendStatus(500);
-    }
-}
+    delete user.confirmPassword;
 
-export const signInHandler = async (req,res) => {
-    const login = res.locals.cleanData;
-    const user = await userHandler.userValidation(login)
-    if(user && bcrypt.compareSync(login.password, user.password)){
-        const token = jwt.sign({id: user.id}, SECRET, {expiresIn: "3d"});
-        return res.json({token, name: user.name}).status(200);
-    }else{
-        return res.sendStatus(401);
-    }
-}
+    const hashPassword = bcrypt.hashSync(user.password, 10);
 
-export const userDataHandler = async (req,res) => {
-    const {id} = res.locals.user;
-    const {rowCount, body} = await userHandler.info(id);
-    if(rowCount === 0) return res.sendStatus(404);
-    return res.send(body).status(200);
-}
+    const success = await userHandler.signUp(user, hashPassword);
+
+    if (success) return res.sendStatus(201);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+};
+
+export const signInHandler = async (req, res) => {
+  const login = res.locals.cleanData;
+  const user = await userHandler.userValidation(login);
+  if (user && bcrypt.compareSync(login.password, user.password)) {
+    const token = jwt.sign({ id: user.id }, SECRET, { expiresIn: "3d" });
+    return res.json({ token, name: user.name }).status(200);
+  } else {
+    return res.sendStatus(401);
+  }
+};
+
+export const userDataHandler = async (req, res) => {
+  const { id } = res.locals.user;
+  const { rowCount, body } = await userHandler.info(id);
+  if (rowCount === 0) return res.sendStatus(404);
+  return res.send(body).status(200);
+};
--- src/middlewares/tokenMiddleware.js
+++ src/middlewares/tokenMiddleware.js
@@ -1,14 +1,14 @@
-import jwt  from "jsonwebtoken";
-const SECRET = process.env.ACCESS_TOKEN_SECRET;
+import jwt from "jsonwebtoken";
+const SECRET = process.env.TOKEN_SECRET;
 
-export async function authenticateToken (req, res, next){
-    const { authorization } = req.headers;
-    const token = authorization?.replace('Bearer ', '');
-    if(token === null) return res.sendStatus(401);
-    jwt.verify(token, SECRET, (err, id) => {
-        if(err) return res.sendStatus(401);
+export async function authenticateToken(req, res, next) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+  if (token === null) return res.sendStatus(401);
+  jwt.verify(token, SECRET, (err, id) => {
+    if (err) return res.sendStatus(401);
 
-        res.locals.user = id;
-        next();
-    });
-};
+    res.locals.user = id;
+    next();
+  });
+}
